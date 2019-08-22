04ec79fb5a4663107c6b4ef13797dd6477fca38a
Maciej Fijałkowski <maciejfijalkowski@MacBook-Air-Maciej.local>
2019-08-22 12:57:53 +0200

    app refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Counter from "./components/counterComponent";
+import NavBar from "./components/navbar";
+import Counters from "./components/countersComponent";
 
 function App() {
   return (
-    <div className="container">
-      <Counter />
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Counters />
+      </main>
+    </React.Fragment>
   );
 }
 
